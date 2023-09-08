c7d1ac75de9f6167701df7a3324ef8e95dcfd60f
Giorgio Tentorio <giorgiotentorio@gmail.com>
2023-09-08 17:25:05 +0200

    Register e login con mysql
    ez mongodb stra un mongoloide ezezezezez

--- cloverz/app.js
+++ cloverz/app.js
@@ -1,71 +1,111 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-require("./config/database").connect();
 const express = require("express");
+const mysql = require("mysql2/promise"); // Import mysql2
 
 const app = express();
 
 app.use(express.json());
 
+// Database configuration
+const dbConfig = {
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+};
+// Create a MySQL pool for handling connections
+const pool = mysql.createPool(dbConfig);
 // Logic goes here
-// importing user context
-const User = require("./model/user");
-
 // Register
 app.post("/register", async (req, res) => {
-
     // Our register logic starts here
     try {
         // Get user input
-        const { first_name, last_name, email, password } = req.body;
+        const { username, password } = req.body;
 
         // Validate user input
-        if (!(email && password && first_name && last_name)) {
+        if (!(username && password)) {
             res.status(400).send("All input is required");
+            return;
         }
 
-        // check if user already exist
-        // Validate if user exist in our database
-        const oldUser = await User.findOne({ email });
+        // Check if user already exists
+        const [rows] = await pool.query("SELECT * FROM utente WHERE username = ?", [
+            username,
+        ]);
 
-        if (oldUser) {
-            return res.status(409).send("User Already Exist. Please Login");
+        if (rows.length > 0) {
+            return res.status(409).send("User Already Exists. Please Login");
         }
 
-        //Encrypt user password
-        encryptedPassword = await bcrypt.hash(password, 10);
-
-        // Create user in our database
-        const user = await User.create({
-            first_name,
-            last_name,
-            email: email.toLowerCase(), // sanitize: convert email to lowercase
-            password: encryptedPassword,
-        });
+        // Encrypt user password
+        const encryptedPassword = await bcrypt.hash(password, 10);
 
         // Create token
+
+        // Create user in the database
+        await pool.query("INSERT INTO utente (username, password) VALUES (?, ?)", [
+            username.toLowerCase(),
+            encryptedPassword,
+        ]);
+
         const token = jwt.sign(
-            { user_id: user._id, email },
+            { username },
             process.env.TOKEN_KEY,
             {
                 expiresIn: "2h",
             }
         );
-        // save user token
-        user.token = token;
 
-        // return new user
-        res.status(201).json(user);
+        // Return new user
+        res.status(201).json({ username, token });
     } catch (err) {
-        console.log(err);
+        console.error(err);
+        res.status(500).send("Internal Server Error");
     }
     // Our register logic ends here
 });
 
 // Login
-app.post("/login", (req, res) => {
-    // our login logic goes here
+app.post("/login", async (req, res) => {
+    // Our login logic goes here
+    try {
+        const { username, password } = req.body;
+
+        // Fetch user from the database
+        const [rows] = await pool.query("SELECT * FROM utente WHERE username = ?", [
+            username,
+        ]);
+
+        if (rows.length === 0) {
+            return res.status(401).send("Invalid credentials");
+        }
+
+        const user = rows[0];
+
+        // Check if the provided password matches the stored hashed password
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordMatch) {
+            return res.status(401).send("Invalid credentials");
+        }
+
+        // Create token
+        const token = jwt.sign(
+            { username },
+            process.env.TOKEN_KEY,
+            {
+                expiresIn: "2h",
+            }
+        );
+
+        res.status(200).json({ username, token, message: "Login effettuato" });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Internal Server Error");
+    }
 });
 
 const auth = require("./middleware/auth");
