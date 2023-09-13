687abb5faddc65d8309a049877b475555f421f8e
falze <dfalzetta@softeam.it>
2023-09-13 12:15:14 +0200

    Login corretto
    la mamma di gio fa la pasta col pollo

--- angular/src/app/login/login.component.ts
+++ angular/src/app/login/login.component.ts
@@ -1,9 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+
+
+  @Output() loginClicked: EventEmitter<void> = new EventEmitter<void>();
+
+  username: string = 'm'; // Dichiarazione di variabili al di fuori della funzione
+  password: string = 'z'; // Dichiarazione di variabili al di fuori della funzione
+  azione: string = '';
+  getJsonValue: any;
+
+  constructor(private router: Router, private http: HttpClient) {
+  }
+
+  public eseguiAzione() {
+    eval(`this.${this.azione}()`);
+}
+
+  getValue(val:string){
+    return val
+   }
+
+  public login(usr: string, psw: string) {
+    // Create an HttpHeaders object with the "Access-Control-Allow-Origin" and "Content-Type" headers
+    const headers = new HttpHeaders({
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json', // Set the content type to JSON
+    });
+
+    // Define the data to be sent in the request body
+    const requestBody = {
+      username: usr, // Usa this.username per ottenere il valore dall'input
+      password: psw, // Usa this.password per ottenere il valore dall'input
+    };
+
+    //console.warn(this.username)
+
+    // Convert the requestBody object to a JSON string
+    const requestBodyJSON = JSON.stringify(requestBody);
+
+    // Define the options for the HTTP POST request, including the headers and the body
+    const httpOptions = {
+      headers: headers,
+      body: requestBodyJSON, // Include the JSON request body here
+    };
+
+    this.http
+      .post('http://localhost:3000/login', requestBodyJSON, httpOptions)
+      .subscribe((data) => {
+        console.log(data);
+        this.getJsonValue = data;
+      });
+  }
+
+
 }
--- angular/src/app/pulsante/pulsante.component.ts
+++ angular/src/app/pulsante/pulsante.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders  } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -10,8 +10,9 @@ import { Router } from '@angular/router';
 })
 export class PulsanteComponent {
   @Input() nome: string = '';
-  @Input() pagina:string='';
-  @Input() azione:string='';
+  @Input() pagina: string = '';
+  @Input() azione: string = '';
+  @Input() parametri: string = '';
 
   public getJsonValue: any;
   public postJsonValue: any;
@@ -19,58 +20,35 @@ export class PulsanteComponent {
   constructor(private router: Router, private http: HttpClient) {}
 
   public eseguiAzione() {
-      eval(`this.${this.azione}()`);
+    if(this.azione!='')
+    {
+      eval(`this.${this.azione}(${this.parametri})`);
+    }
+      
   }
 
   cambiaPagina() {
-    this.router.navigate(['/',this.pagina]);
+    this.router.navigate(['/', this.pagina]);
   }
 
   public getMethod() {
     // Create an HttpHeaders object with the "Access-Control-Allow-Origin" header
     const headers = new HttpHeaders({
-      'Access-Control-Allow-Origin': '*'
+      'Access-Control-Allow-Origin': '*',
     });
-  
+
     // Define the options for the HTTP GET request, including the headers
     const httpOptions = {
-      headers: headers
+      headers: headers,
     };
     // Send the GET request with the specified headers
-    this.http.get('http://localhost:3000/welcome', httpOptions).subscribe((data) => {
-      console.log(data)
-      this.getJsonValue = data
-      // console.log("aia")
-      // console.log(this.getJsonValue)
-    });
-  }
-
-  public login() {
-  // Create an HttpHeaders object with the "Access-Control-Allow-Origin" and "Content-Type" headers
-    const headers = new HttpHeaders({
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/json' // Set the content type to JSON
-    });
-
-    // Define the data to be sent in the request body
-    const requestBody = {
-      "username": 'admin',
-      "password": 'admin'
-    };
-
-    // Convert the requestBody object to a JSON string
-    const requestBodyJSON = JSON.stringify(requestBody);
-
-    // Define the options for the HTTP POST request, including the headers and the body
-    const httpOptions = {
-      headers: headers,
-      body: requestBodyJSON // Include the JSON request body here
-    };
-
-    this.http.post('http://localhost:3000/login', requestBodyJSON, httpOptions).subscribe((data) => {
-    console.log(data);
-    this.getJsonValue = data;
-    });
-
+    this.http
+      .get('http://localhost:3000/welcome', httpOptions)
+      .subscribe((data) => {
+        console.log(data);
+        this.getJsonValue = data;
+        // console.log("aia")
+        // console.log(this.getJsonValue)
+      });
   }
 }
