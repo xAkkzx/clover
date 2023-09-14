111f408805147d742203de8cf11e7db7217ced35
Giorgio Tentorio <giorgiotentorio@gmail.com>
2023-09-14 14:33:36 +0200

    pranzetto buono direi
    ci stava ci stava

--- angular/src/app/gpt/gpt.component.ts
+++ angular/src/app/gpt/gpt.component.ts
@@ -1,15 +1,20 @@
-import { Component, EventEmitter, Output, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { FormGroup, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { CustomToastrService } from '../custom-toastr.service';
+import { GlobalService } from '../global.service';
+import { TextfieldComponent } from '../textfield/textfield.component';
 
 @Component({
   selector: 'app-gpt',
   templateUrl: './gpt.component.html',
-  styleUrls: ['./gpt.component.css'],
+  styleUrls: ['./gpt.component.css']
 })
-export class GptComponent implements AfterViewInit {
+export class GptComponent implements AfterViewInit{
+  token : string;
+  @ViewChild(TextfieldComponent, { static: false }) textfieldRic!: TextfieldComponent;
   chatMessages: { text: string; type: string }[] = [];
   
   @Output() loginClicked: EventEmitter<void> = new EventEmitter<void>();
@@ -24,14 +29,17 @@ export class GptComponent implements AfterViewInit {
     private http: HttpClient,
     private toastr: ToastrService,
     private customToastrService: CustomToastrService,
+    private globalService: GlobalService,
     private el: ElementRef
   ) {
+    this.token = this.globalService.getGlobalVariable();
   }
-
-  @ViewChild('chatContainer') private chatContainer!: ElementRef;
+   @ViewChild('chatContainer') private chatContainer!: ElementRef;
 
   ngAfterViewInit() {
-    // Access the nativeElement here
+    if (this.textfieldRic) {
+      this.textfieldRic.resetValue();
+    }
     if (this.chatContainer && this.chatContainer.nativeElement) {
       console.log("aa");
       const chatContainerElement = this.chatContainer.nativeElement;
@@ -49,44 +57,100 @@ export class GptComponent implements AfterViewInit {
     return val;
   }
 
-  public gptRes(nomeDb: string, richiesta: string) {
+  public chat(nomeDbz : any, tipoDbz : any, richiestaz : any){
 
-    console.log(nomeDb, richiesta);
+    const headers = new HttpHeaders({
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json', // Set the content type to JSON
+      'x-access-token': this.token
+    });
 
-    // Split the request into lines with a maximum length of 25 characters
+    const requestBody = {
+      nomeDb : nomeDbz,
+      tipoDb : tipoDbz,
+      richiesta: richiestaz // Usa this.password per ottenere il valore dall'input
+    };
+    // Convert the requestBody object to a JSON string
+    const requestBodyJSON = JSON.stringify(requestBody);
+
+    const httpOptions = {
+      headers: headers,
+      body: requestBodyJSON, // Include the JSON request body here
+    };
+
+    this.ngAfterViewInit();
     const maxLineLength = 45;
     const lines: string[] = [];
     let currentLine = '';
 
-    for (let i = 0; i < richiesta.length; i++) {
+    for (let i = 0; i < richiestaz.length; i++) {
       if (currentLine.length === maxLineLength) {
         // If the current line reaches the maximum length, add it to the lines array and start a new line
         lines.push(currentLine);
         currentLine = '';
       }
-      currentLine += richiesta[i];
+      currentLine += richiestaz[i];
     }
-
-    // Add the last line to the lines array if it's not empty
     if (currentLine.length > 0) {
       lines.push(currentLine);
     }
-
-    // Join the lines with '\n' to create the formatted request
     const formattedRequest = lines.join('\n');
-
-    // Simulate a response (you can replace this with the actual response logic)
     const response = 'This is a simulated response for:\n' + formattedRequest;
     this.chatMessages.push({ text: formattedRequest, type: 'request' });
     this.chatMessages.push({ text: response, type: 'response' });
-
-    // Scroll to the bottom of the chat container
     setTimeout(() => {
       console.log("A");
       this.scrollToBottom();
     });
+
+    this.http
+      .post('http://localhost:3000/chat', requestBodyJSON, {
+        ...httpOptions,
+        observe: 'response',
+      })
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+          
+          if (response.status === 200) {
+            // this.toastr.success('Login effettuato', 'Benvenuto!', { positionClass: 'toast-bottom-right'});
+            console.log('Risposta arrivata');
+            // this.router.navigate(['/', 'gpt']);
+          }
+        },
+        error: (error) => {
+          if (error.status===500)
+              if(error.error.includes('Sign In'))
+              {
+                this.customToastrService.showErrorWithLink(
+                  error.error.replace("Sign In", ""),
+                  'Sign In',
+                  'http://localhost:4200/register'
+                );
+              }else{
+                this.toastr.error(error.error, 'Error', { positionClass: 'toast-bottom-right'});
+              }
+          if(error.status === 401)
+          {
+            this.customToastrService.showErrorWithLink(
+                  error.error.replace("Sign In", ""),
+                  'Sign In',
+                  'http://localhost:4200/login'
+                );
+          }
+            
+          console.log(error.status);
+          console.error('Errore durante la richiesta:', error);
+          // Puoi gestire gli errori di rete o altri errori qui
+        },
+      });
   }
 
+  public esci()
+  {
+    this.globalService.setGlobalVariable("");
+    this.router.navigate(['/', 'login']);
+  }
   private scrollToBottom() {
       if (this.chatContainer && this.chatContainer.nativeElement) {
         console.log("chatContainer and nativeElement exist");
@@ -98,4 +162,3 @@ export class GptComponent implements AfterViewInit {
       }
   }
 }
-
